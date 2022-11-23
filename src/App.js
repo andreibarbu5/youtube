2eb285d10ab86d4008b0af03893555a39b991eab
BarbuAndreiIulian <83908356+andreibarbu5@users.noreply.github.com>
2022-11-23 19:59:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Content from "./components/Content";
+import Navbar from "./components/Navbar";
+import Topics from "./components/Topics";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Navbar */}
+      <Navbar />
+      <div className="flex">
+        {/* Sidebar */}
+        <Sidebar />
+        <div className=" ">
+          {/* Topics */}
+          <Topics />
+          {/* Content */}
+          <Content />
+        </div>
+      </div>
+
+      {/* Shorts */}
     </div>
   );
 }
